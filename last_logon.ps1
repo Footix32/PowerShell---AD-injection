cc52675a8a865992aea78acc5c5ab7009e15e1da
Footix32 <stephane.fortin@live.fr>
2025-04-02 16:00:15 +0200

    Update last_logon.ps1
    modify

--- last_logon.ps1
+++ last_logon.ps1
@@ -1,38 +1,44 @@
-# Retrieve the list of all Domain Controllers in the AD domain
-$DCList = Get-ADDomainController -Filter * | Sort-Object Name | Select-Object Name
-
 # Target user (SamAccountName)
 $TargetUser = "admin.fb"
 
+# Retrieve the list of all Domain Controllers in the AD domain
+$DCList = Get-ADDomainController -Filter * | Sort-Object Name | Select-Object Name
+
 # Initialize LastLogon to $null as the starting point
 $TargetUserLastLogon = $null
-    
-Foreach($DC in $DCList){
 
-        $DCName = $DC.Name
- 
-        Try {
-            
-            # Retrieve the lastLogon attribute value from each Domain Controller (one DC at a time)
-            $LastLogonDC = Get-ADUser -Identity $TargetUser -Properties lastLogon -Server $DCName
+# Ensure that the Domain Controllers are available
+if ($DCList.Count -eq 0) {
+    Write-Host "No Domain Controllers found!" -ForegroundColor Red
+    exit
+}
 
-            # Convert the value to DateTime format
-            $LastLogon = [Datetime]::FromFileTime($LastLogonDC.lastLogon)
+# Loop through each Domain Controller
+foreach ($DC in $DCList) {
+    $DCName = $DC.Name
 
-            # If the obtained value is more recent than the one stored in $TargetUserLastLogon,
-            # update the variable: this ensures that we have the most recent lastLogon by the end of the process
-            If ($LastLogon -gt $TargetUserLastLogon)
-            {
-                $TargetUserLastLogon = $LastLogon
-            }
- 
-            # Clean the variable
-            Clear-Variable LastLogon
-            }
+    Try {
+        # Retrieve the lastLogon attribute value from each Domain Controller (one DC at a time)
+        $LastLogonDC = Get-ADUser -Identity $TargetUser -Properties lastLogon -Server $DCName
 
-        Catch {
-            Write-Host $_.Exception.Message -ForegroundColor Red
+        # Check if user exists on the DC
+        if (-not $LastLogonDC) {
+            Write-Host "User $TargetUser not found on $DCName." -ForegroundColor Red
+            continue
         }
+
+        # Convert the value to DateTime format
+        $LastLogon = [Datetime]::FromFileTime($LastLogonDC.lastLogon)
+
+        # If the obtained value is more recent than the one stored in $TargetUserLastLogon,
+        # update the variable: this ensures that we have the most recent lastLogon by the end of the process
+        if ($LastLogon -gt $TargetUserLastLogon) {
+            $TargetUserLastLogon = $LastLogon
+        }
+    }
+    Catch {
+        Write-Host "Error retrieving data from $DCName: $_" -ForegroundColor Red
+    }
 }
 
 Write-Host "Last logon date for ${TargetUser}:"
