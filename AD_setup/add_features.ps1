972136437b8da18018fd6a2ac5251e023a1590ca
Footix32 <stephane.fortin@live.fr>
2025-04-02 15:34:32 +0200

    Update add_features.ps1
    MODIFY

--- AD_setup/add_features.ps1
+++ AD_setup/add_features.ps1
@@ -2,9 +2,21 @@
 
 $FeatureList = @("RSAT-AD-Tools", "AD-Domain-Services", "DNS")
 
+# Get OS version
+$OSVersion = (Get-CimInstance Win32_OperatingSystem).Version
+Write-Output "OS Version: $OSVersion"
+
+# Check if the OS is compatible (example for Windows Server 2016+)
+if ($OSVersion -lt "10.0.14393") {
+    Write-Output "This script is intended for Windows Server 2016 or later."
+    return
+}
+
 foreach ($Feature in $FeatureList) {
 
+   # Get feature state
    $FeatureState = Get-WindowsFeature -Name $Feature
+
    if ($FeatureState.InstallState -eq "Available") {
 
      Write-Output "Feature $Feature will be installed now!"
